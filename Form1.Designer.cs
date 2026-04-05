593cdd012063fd423b8bcdd9d4f41bc64c06205c
cyalchvadym-collab <c.yalch.vadym@student.uzhnu.edu.ua>
2026-04-05 17:30:27 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab8_ButtonEscaper
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			escapeButton = new Button();
+			infoLabel = new Label();
+			scoreLabel = new Label();
+			resetButton = new Button();
+			difficultyButton = new Button();
+			speedTrackBar = new TrackBar();
+			crazyModeCheck = new CheckBox();
+			exitButton = new Button();
+			((System.ComponentModel.ISupportInitialize)speedTrackBar).BeginInit();
+			SuspendLayout();
+			// 
+			// escapeButton
+			// 
+			escapeButton.Location = new Point(415, 247);
+			escapeButton.Name = "escapeButton";
+			escapeButton.Size = new Size(150, 46);
+			escapeButton.TabIndex = 0;
+			escapeButton.Text = "Спіймай мене!";
+			escapeButton.UseVisualStyleBackColor = true;
+			escapeButton.Click += escapeButton_Click;
+			escapeButton.MouseEnter += escapeButton_MouseEnter;
+			// 
+			// infoLabel
+			// 
+			infoLabel.AutoSize = true;
+			infoLabel.Location = new Point(12, 9);
+			infoLabel.Name = "infoLabel";
+			infoLabel.Size = new Size(413, 32);
+			infoLabel.TabIndex = 1;
+			infoLabel.Text = "Наведіть курсор на червону кнопку";
+			// 
+			// scoreLabel
+			// 
+			scoreLabel.AutoSize = true;
+			scoreLabel.Location = new Point(12, 64);
+			scoreLabel.Name = "scoreLabel";
+			scoreLabel.Size = new Size(145, 32);
+			scoreLabel.TabIndex = 2;
+			scoreLabel.Text = "Спіймано: 0";
+			// 
+			// resetButton
+			// 
+			resetButton.Location = new Point(7, 111);
+			resetButton.Name = "resetButton";
+			resetButton.Size = new Size(150, 46);
+			resetButton.TabIndex = 3;
+			resetButton.Text = "Скинути";
+			resetButton.UseVisualStyleBackColor = true;
+			resetButton.Click += resetButton_Click;
+			// 
+			// difficultyButton
+			// 
+			difficultyButton.Location = new Point(163, 105);
+			difficultyButton.Name = "difficultyButton";
+			difficultyButton.Size = new Size(150, 72);
+			difficultyButton.TabIndex = 4;
+			difficultyButton.Text = "Складність: Нормальнo";
+			difficultyButton.UseVisualStyleBackColor = true;
+			difficultyButton.Click += difficultyButton_Click;
+			// 
+			// speedTrackBar
+			// 
+			speedTrackBar.Location = new Point(397, 396);
+			speedTrackBar.Name = "speedTrackBar";
+			speedTrackBar.Size = new Size(208, 90);
+			speedTrackBar.TabIndex = 5;
+			speedTrackBar.ValueChanged += speedTrackBar_ValueChanged;
+			// 
+			// crazyModeCheck
+			// 
+			crazyModeCheck.AutoSize = true;
+			crazyModeCheck.Location = new Point(611, 411);
+			crazyModeCheck.Name = "crazyModeCheck";
+			crazyModeCheck.Size = new Size(274, 36);
+			crazyModeCheck.TabIndex = 6;
+			crazyModeCheck.Text = "Божевільний режим";
+			crazyModeCheck.UseVisualStyleBackColor = true;
+			crazyModeCheck.CheckedChanged += crazyModeCheck_CheckedChanged;
+			// 
+			// exitButton
+			// 
+			exitButton.Location = new Point(926, 471);
+			exitButton.Name = "exitButton";
+			exitButton.Size = new Size(150, 67);
+			exitButton.TabIndex = 7;
+			exitButton.Text = "Вихід";
+			exitButton.UseVisualStyleBackColor = true;
+			exitButton.Click += exitButton_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(13F, 32F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.LightBlue;
+			ClientSize = new Size(1095, 547);
+			Controls.Add(exitButton);
+			Controls.Add(crazyModeCheck);
+			Controls.Add(speedTrackBar);
+			Controls.Add(difficultyButton);
+			Controls.Add(resetButton);
+			Controls.Add(scoreLabel);
+			Controls.Add(infoLabel);
+			Controls.Add(escapeButton);
+			Name = "Form1";
+			Text = "Варіант 8 - Кнопка-утікачка";
+			MouseClick += Form1_MouseClick;
+			((System.ComponentModel.ISupportInitialize)speedTrackBar).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button escapeButton;
+		private Label infoLabel;
+		private Label scoreLabel;
+		private Button resetButton;
+		private Button difficultyButton;
+		private TrackBar speedTrackBar;
+		private CheckBox crazyModeCheck;
+		private Button exitButton;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab8_ButtonEscaper
+{
+	public partial class Form1 : Form
+	{
+		private System.Windows.Forms.Timer moveTimer;
+		private float speed = 5f;
+		private float acceleration = 1.2f;
+		private int targetX, targetY;
+		private int margin = 50;
+		private int catchCount = 0;
+		private int missCount = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+			InitializeTimer();
+		}
+
+		private void InitializeTimer()
+		{
+			moveTimer = new System.Windows.Forms.Timer();
+			moveTimer.Interval = 16;
+			moveTimer.Tick += MoveTimer_Tick;
+		}
+
+		private void escapeButton_MouseEnter(object sender, EventArgs e)
+		{
+			if (crazyModeCheck.Checked)
+			{
+				TeleportButton();
+			}
+			else
+			{
+				CalculateEscapePosition();
+				if (!moveTimer.Enabled)
+				{
+					speed = speedTrackBar.Value;
+					moveTimer.Start();
+				}
+			}
+		}
+
+		private void escapeButton_Click(object sender, EventArgs e)
+		{
+			catchCount++;
+			UpdateScore();
+
+			MessageBox.Show($"Вітаю! Ви спіймали кнопку!\nВсього спіймано: {catchCount}",
+				"Перемога!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+			speed = speedTrackBar.Value;
+			escapeButton.Location = new Point(400, 300);
+		}
+
+		private void TeleportButton()
+		{
+			Random rand = new Random();
+			int newX = rand.Next(margin, this.ClientSize.Width - margin - escapeButton.Width);
+			int newY = rand.Next(margin + 100, this.ClientSize.Height - margin - escapeButton.Height);
+			escapeButton.Location = new Point(newX, newY);
+			missCount++;
+			UpdateScore();
+		}
+
+		private void CalculateEscapePosition()
+		{
+			Point cursorPos = this.PointToClient(Cursor.Position);
+			Point buttonCenter = new Point(
+				escapeButton.Location.X + escapeButton.Width / 2,
+				escapeButton.Location.Y + escapeButton.Height / 2
+			);
+
+			int dx = buttonCenter.X - cursorPos.X;
+			int dy = buttonCenter.Y - cursorPos.Y;
+			double distance = Math.Sqrt(dx * dx + dy * dy);
+			if (distance < 1) distance = 1;
+
+			double escapeDirX = dx / distance;
+			double escapeDirY = dy / distance;
+
+			Random rand = new Random();
+			escapeDirX += (rand.NextDouble() - 0.5) * 0.5;
+			escapeDirY += (rand.NextDouble() - 0.5) * 0.5;
+
+			int escapeDistance = 200 + rand.Next(100);
+			targetX = buttonCenter.X + (int)(escapeDirX * escapeDistance);
+			targetY = buttonCenter.Y + (int)(escapeDirY * escapeDistance);
+
+			targetX = Math.Max(margin, Math.Min(this.ClientSize.Width - margin - escapeButton.Width, targetX));
+			targetY = Math.Max(margin + 100, Math.Min(this.ClientSize.Height - margin - escapeButton.Height, targetY));
+		}
+
+		private void MoveTimer_Tick(object sender, EventArgs e)
+		{
+			int currentX = escapeButton.Location.X;
+			int currentY = escapeButton.Location.Y;
+
+			int diffX = targetX - currentX;
+			int diffY = targetY - currentY;
+			double distance = Math.Sqrt(diffX * diffX + diffY * diffY);
+
+			if (distance < 5)
+			{
+				moveTimer.Stop();
+				speed = speedTrackBar.Value;
+				return;
+			}
+
+			speed = Math.Min(speed * acceleration, 30f);
+
+			double moveRatio = speed / distance;
+			int newX = currentX + (int)(diffX * moveRatio);
+			int newY = currentY + (int)(diffY * moveRatio);
+
+			escapeButton.Location = new Point(newX, newY);
+		}
+
+		private void resetButton_Click(object sender, EventArgs e)
+		{
+			catchCount = 0;
+			missCount = 0;
+			UpdateScore();
+			speed = speedTrackBar.Value;
+			escapeButton.Location = new Point(400, 300);
+		}
+
+		private void difficultyButton_Click(object sender, EventArgs e)
+		{
+			if (acceleration == 1.2f)
+			{
+				acceleration = 1.05f;
+				difficultyButton.Text = "Складність: Легко";
+				difficultyButton.BackColor = Color.LightGreen;
+			}
+			else if (acceleration == 1.05f)
+			{
+				acceleration = 1.3f;
+				difficultyButton.Text = "Складність: Складно";
+				difficultyButton.BackColor = Color.Orange;
+			}
+			else
+			{
+				acceleration = 1.2f;
+				difficultyButton.Text = "Складність: Нормально";
+				difficultyButton.BackColor = Color.Yellow;
+			}
+		}
+
+		private void exitButton_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void speedTrackBar_ValueChanged(object sender, EventArgs e)
+		{
+			speed = speedTrackBar.Value;
+		}
+
+		private void crazyModeCheck_CheckedChanged(object sender, EventArgs e)
+		{
+			if (crazyModeCheck.Checked)
+			{
+				escapeButton.BackColor = Color.Purple;
+				escapeButton.Text = "ТЕЛЕПОРТ!";
+			}
+			else
+			{
+				escapeButton.BackColor = Color.Tomato;
+				escapeButton.Text = "Спіймай мене!";
+			}
+		}
+
+		private void Form1_MouseClick(object sender, MouseEventArgs e)
+		{
+			missCount++;
+			UpdateScore();
+		}
+
+		private void UpdateScore()
+		{
+			scoreLabel.Text = $"Спіймано: {catchCount} | Промахів: {missCount}";
+		}
+	}
+}
